b991d5e8cb7e59132808c2cc631acff6c3614ca0
github username <gregm9999@gmail.com>
2019-09-07 16:05:38 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][key]
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key] << new_value.to_s
+      end
+    end
+  end
+  pigeon_list
 end
